cdb2f31283dd2da16ca48930998b820ead6b6242
fozzie77 <54290959+fozzie77@users.noreply.github.com>
2019-10-15 23:07:22 -0600

    Styled buttons, header, and positions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -33,134 +33,97 @@ function initialize() {
   }
 
   var randomWord = ((words[getRandom10(10)]).toUpperCase());
-    console.log (randomWord); //random word
-  }
+    document.getElementById('ph').innerHTML += (randomWord);//random word
+
   for (i=0; i<randomWord.length; i++) //populates spaces for letters
   {document.getElementById('answerBar').innerHTML +='<img id=space src="assets/images/space.png">'
   document.getElementById("init").style.visibility = "hidden"//hides button
-}
-
+    }
+  }
+  
 //Captures input from click or push and stores in var "letter"
 
-  function clicked(clicked_id){
-  j=(clicked_id);
-  letter=(j.toUpperCase());
-  console.log(letter);
-  }
-
   document.onkeypress = function (event) {
-  event = event || window.event;
-  i = event.keyCode || event.which;
-  j = String.fromCharCode(i);
-  letter=(j.toUpperCase());
-  console.log(letter);
-  
-  //would like to make this new function called by either click or press
-  if (randomWord.indexOf(letter)>-1)
-  {console.log(randomWord.indexOf)
-}
+  letter = (String.fromCharCode(event.keyCode).toUpperCase());
+  document.getElementById('let').innerHTML += (letter);
+  }
+
+  function clicked(clicked_id){
+    letter = (clicked_id.toUpperCase());
+    document.getElementById('let').innerHTML += (letter);
+    
+  //need to bring "clicked" into this.
+  guesses = [];
+  ins = (document.getElementById('ph').innerHTML);
+  if (ins.includes(letter)===true) {
+    guesses.push (letter);}
+
   else {
-    console.log (console.log(randomWord.indexOf));
+    console.log ("nope");
   }
 }
-  // if (code.indexOf(randomWord).includes(letter)) {
-  //   console.log("yup");}
-  //    else {
-  //     console.log("nope");
-  //   }
-  //  }
+
+        //   if (((isInArray == true)&&(guesses.includes(String.fromCharCode(letter)))===false)
+        //       &&(uniqueLetters.includes(String.fromCharCode(tGuess)))===false){/*selection must be a letter, not already guessed, and not in the answer*/
+        // guesses.push (String.fromCharCode(tGuess)); /*pushes letter from tGuess into "guesses" array*/
+        //  console.log (guesses);
+         
+        //   document.getElementById("beam").style.visibility = "visible";
+        //   setTimeout(function(){ document.getElementById("beam").style.visibility = "hidden" }, 1000);
+        //   i=(guesses.length);
+        //   document.getElementById("cow"+[i]).style.visibility = "hidden"
+        //   }
+
+        //   if (((isInArray == true)&&(guesses.includes(String.fromCharCode(tGuess)))===false)
+        //   &&(uniqueLetters.includes(String.fromCharCode(tGuess)))===true){/*selection must be a letter, not already guessed, and in the answer*/
+        //   vanna.push (String.fromCharCode(tGuess)); /*pushes letter from tGuess into "guesses" array*/
+        //   console.log (vanna);
 
 
-  
- 
- 
+        //   if (isInArray == false) {/*ensures only valid guesses are counted*/
+        //     alert ("not valid keystroke");
 
-//  if (randomWord.includes(letter)===true) {
-//   console.log("yup");}
-//   else {
-//     console.log("nope");
-//   }
-//Checks var "letter" against var "randomWord"
- 
-
-
-
-//  tGuess = (event.keyCode)||(integer),   
-//  console.log (tGuess),
-//  guess = String.fromCharCode(tGuess);
-//   console.log (guess);
-//  isInArray = letterArray.includes(tGuess); 
-
-
-//Populates initial images
-
-  
+        // if (guesses.length > 9) { /*Too many guess triggers game's end*/
+        // alert ("You Fail!");
+      
 // function aBar() {
 //   for (var i; i<randomWord.length-1; i++) {document.getElementById('answerBar').innerHTML += "_";}}
 
 // var answerArray = [];
 // for (var i = 0; i < randomWord.length; i++) {
 //   answerArray[i] = "_";
-// }
-
-//   function clicked(clicked_id){
-//       current=(clicked_id);
-//   }
-
-    //var wordSplit = [] empty array filled by letters in randomWord
-//console.log (wordSplit);
-
-//     for (numPos=0; numPos<wLength; numPos++) {/*converts randomWord into array of letters*/
-//       wordSplit.push(randomWord[numPos]);
-//     }
 
 //     const unique = (value, index, self) => {/*creates a list of unique letters in the word called "uniqueLetters" for easy comparison*/
 //       return self.indexOf(value) === index
 //     }
 //     const uniqueLetters = wordSplit.filter(unique)
     
-
 //     var answerArray = [];
 //       for (var i = 0; i < randomWord.length; i++) {
 //         answerArray[i] = "_";
 //       }
 //     var remainingLetters = randomWord.length;
-//     console.log (answerArray);
 
 // /* Capturing guesses. Logs in array "guesses" as keyCodes. */
  
 //     var letterArray = Array.from(new Array(26), (x,i) => i + 65); /*gets all numbers correlating to letter keycodes and names the variables letterArray */
 
-//     console.log (letterArray); //test line
-
 //     document.onkeyup = letterSelect; /*listens for keystrokes*/
 
 //     var guesses = []; /*empty array filled by letters in randomWord*/
 
-//     console.log (guesses)
-
 //     var vanna = []; /*empty array filled by guesses that correspond with randomWord*/
 
-//     function clicked(clicked_id){//pulls id from button and converts string to number before running letterSelect.
-//       integer = parseInt(clicked_id, 10);
-//       console.log (integer);
-//       letterSelect();
-//      }
 
 // /*Captures keycode for key pushed. If valid, adds to variable "guesses". If not, alerts player.*/
 //     document.onkeyup = letterSelect
 // function letterSelect() {
-//       tGuess = (event.keyCode)||(integer), 
-//       console.log (tGuess),//test line
-//       guess = String.fromCharCode(tGuess);//converting keyCode to string var "guess"
-//       console.log (guess);
+//     
 //       isInArray = letterArray.includes(tGuess); /*checks that tGuess is a letter (in letterArray)*/
-//       /*console.log (isInArray);test line*/
         
 //         if (((isInArray == true)&&(guesses.includes(String.fromCharCode(tGuess)))===false)
 //               &&(uniqueLetters.includes(String.fromCharCode(tGuess)))===false){/*selection must be a letter, not already guessed, and not in the answer*/
 //         guesses.push (String.fromCharCode(tGuess)); /*pushes letter from tGuess into "guesses" array*/
-//          console.log (guesses);
          
 //           document.getElementById("beam").style.visibility = "visible";
 //           setTimeout(function(){ document.getElementById("beam").style.visibility = "hidden" }, 1000);
@@ -171,43 +134,22 @@ function initialize() {
 //           if (((isInArray == true)&&(guesses.includes(String.fromCharCode(tGuess)))===false)
 //           &&(uniqueLetters.includes(String.fromCharCode(tGuess)))===true){/*selection must be a letter, not already guessed, and in the answer*/
 //           vanna.push (String.fromCharCode(tGuess)); /*pushes letter from tGuess into "guesses" array*/
-//           console.log (vanna);
-//           }
 
 //           if (isInArray == false) {/*ensures only valid guesses are counted*/
 //             alert ("not valid keystroke");
-//           }
 
 //         if (guesses.length > 9) { /*Too many guess triggers game's end*/
 //         alert ("You Fail!");
 
-//       };
-//       // console.log (String.fromCharCode(tGuess));
-//       // console.log (guesses);
-
 //       /* Comparing guesses (var guesses) to words (var uniqueLetters)*/
 //       if (wordSplit.includes(String.fromCharCode(tGuess))&&(vanna.includes(String.fromCharCode(tGuess)))===false){
 //         vanna.push(String.fromCharCode(tGuess));h
-   
-//         }
-//         console.log (uniqueLetters);
-//         console.log (vanna);
+//     
 //         if (uniqueLetters.length===vanna.length) {/*comparing lengths of two arrays--no need to check specific characters*/
 //         alert ("You Win!"); 
 //           winCounter++
-//           console.log (winCounter);
-//         } 
+
 //         for (var j = 0; j < randomWord.length; j++) {
 //           if (randomWord[j] === guess) {
 //            answerArray[j] = guess;
 //           remainingLetters--;
-//            }
-//         console.log (answerArray);
-        
-//           }
-          
- 
-
-//      }
-    
-
