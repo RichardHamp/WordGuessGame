f4c7d40d2e3af29b67a885cb901b000643c10b0f
fozzie77 <54290959+fozzie77@users.noreply.github.com>
2019-10-19 08:27:52 -0600

    reset button added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,16 +2,15 @@
 var randomWord =[];
 var letter=[];
 var lettersGuessed=0;
-
 var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-//Global Function
+
+//Global Functions
 function getRandom10(max) {//random number 1-10
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-function keyed(a) {
-  letter = (a);
-  console.log (letter);
+function clearing(elementID){//zeros out div
+    document.getElementById(elementID).innerHTML = "";
 }
 
 function win () {
@@ -26,21 +25,9 @@ function lose () {
   document.getElementById("init").style.visibility = "visible"//hides button
 }
 
-function answers() {document.getElementById('answerBar').innerHTML +='<img id=space'+[i]+' class="space responsive" src="assets/images/space.png">'
-}
-
+//On Win load
 window.onload = function(){
-  
-  //loading alphabet
-  
-  //  onclick="clicked(this.id)" document.getElementById("'+i+'").src="assets/images/Alphabet/'+alphabet[i]+'b.png">'
-    //'<img class="ibtn" input="" type="image" src="assets/images/Alphabet/'+alphabet[j]+'.png" button id="'+alphabet[i]+'"onclick="document.getElementById("'+i+'").src="assets/images/space.png">'
-  
-  // //loading cows
-  // for (i = 1; i < 10; i++) {document.getElementById('cows').innerHTML += 
-  //   '<img class="cow" id="cow'+i+'" src="assets/images/happy.png" alt="cows">';
-  // }
-  //loading flying saucer and beam (hidden)
+
   document.getElementById('gameName').innerHTML +=
     
     '<img id="beam" class="responsive" src="assets/images/shipandbeam.png">'+
@@ -50,33 +37,34 @@ window.onload = function(){
 //Initialize button
 
 function initialize() { 
-  var words = ["bob", "rick", "jo", "vicki", "katie", "Felipe", "Sophia", "Linda", "Sebastian", "Albert Einstein"];/*words needed to guess for game*/
-  
+  //Resetting Variables
+  clearing ('cows');
+  clearing ('win');
+  clearing ('lose');
+  clearing ('bgroup');
+  clearing ('answerBar');
+  lettersGuessed = 0;
+  var words = ["bob", "rick", "jo", "vicki", "katie", "Felipe", "Sophia", "Linda", "Sebastian", "Albert Einstein"];
   var cowsLeft=9;
+  var randomWord = ((words[getRandom10(10)]).toUpperCase());    
+
+  //Resetting Images
+  for (i=0; i<randomWord.length; i++){ //populates spaces for letters
+    document.getElementById('answerBar').innerHTML +='<img id=space'+[i]+' class=space src="assets/images/space.png">'
+    document.getElementById("init").style.visibility = "hidden"//hides button
+      }
 
   document.getElementById('cows').innerHTML += '<img class="cows responsive" input="">'
 
-  for (i = 1; i < 10; i++) {document.getElementById('cows').innerHTML += 
-    '<img class="cow" id="cow'+i+'" src="assets/images/happy.png" alt="cows">';
+  for (i = 1; i < 10; i++) {
+    document.getElementById('cows').innerHTML += '<img class="cow" id="cow'+i+'" src="assets/images/happy.png" alt="cows">';
   }
-
-  document.getElementById('score').innerHTML +="0";//sets current global score to 0
-  console.log (score)
   
-  var randomWord = ((words[getRandom10(10)]).toUpperCase());
-    //document.getElementById('ph').innerHTML += (randomWord);//random word & sets to div ph
-    console.log (ph);
-
-  for (i=0; i<randomWord.length; i++) //populates spaces for letters
-  {document.getElementById('answerBar').innerHTML +='<img id=space'+[i]+' class=space src="assets/images/space.png">'
-  document.getElementById("init").style.visibility = "hidden"//hides button
-    }
-   
   console.log (randomWord)
 
-  //Captures input from click or push and stores in var "letter"
+  //Captures input from key and stores in var "letter"
   for (i = 0; i < 26; i++) {document.getElementById('bgroup').innerHTML += 
-  '<img class="ibtn responsive" input=type="image" src="assets/images/Alphabet/'+alphabet[i]+'.png" button id="'+alphabet[i]+'" onclick=keyed('+alphabet[i]+')>'
+    '<img class="ibtn responsive" input=type="image" src="assets/images/Alphabet/'+alphabet[i]+'.png" button id="'+alphabet[i]+'" onclick=keyed('+alphabet[i]+')>'
 
   document.onkeypress = function (event) {
     guesses=[]
@@ -89,12 +77,11 @@ function initialize() {
       }
       else {guesses.push (letter);
         console.log (guesses);
+        console.log (lettersGuessed);
+        console.log (randomWord);
       }
 
   if (randomWord.indexOf(letter) > -1){
-
-   
-
     for (j=0;j<randomWord.length;j++) {
       if (randomWord[j] == letter) {
         document.getElementById('space'+[j]+'').src = 'assets/images/Alphabet/'+letter+'.png';
@@ -115,142 +102,20 @@ function initialize() {
       lose();
     }
   }
-  
 }
-  }
+}
   
 
-
-
-  // else {
-  //   document.getElementById("beam").style.visibility = "visible";
-  //   setTimeout(function(){ document.getElementById("beam").style.visibility = "hidden" }, 1000);
-  //   i=(guesses.length);
-  //   document.getElementById('cow'+[i]).style.visibility = "hidden";}
-
-  //   guess_array.splice(index, [i], letter);
-	// 		guess_array.splice(randomWord.lastIndexOf(letter), 1, letter);
-	// 		console.log(guess_array);
-  // }
-  
-
-  // function guess(letter){
-	// 	var index = randomWord.indexOf(letter);
-	// 	if(randomWord.indexOf(letter) > -1){
-	// 		guess_array.splice(index, 1, letter);
-	// 		guess_array.splice(randomWord.lastIndexOf(letter), 1, letter);
-	// 		console.log(guess_array);
-
-
-
-  //checks var = letter (let) against word and stores in guesses
-  //ins = (document.getElementById('ph').innerHTML);
-  
-  // if (randomWord.includes(letter)===true) {
-  //   ;
-  //   console.log ("yup")}
-  // else {
-  //   console.log ("nope");}
-
-    // guesses.push (letter);
-    // document.getElementById("beam").style.visibility = "visible";
-    //       setTimeout(function(){ document.getElementById("beam").style.visibility = "hidden" }, 1000);
-    //       i=(guesses.length);
-    //       document.getElementById('cow'+[i]).style.visibility = "hidden";}
-        
-            
-
-
-
-
-
-        //   if (((isInArray == true)&&(guesses.includes(String.fromCharCode(letter)))===false)
-        //       &&(uniqueLetters.includes(String.fromCharCode(tGuess)))===false){/*selection must be a letter, not already guessed, and not in the answer*/
-        // guesses.push (String.fromCharCode(tGuess)); /*pushes letter from tGuess into "guesses" array*/
-        //  console.log (guesses);
-         
-        //   document.getElementById("beam").style.visibility = "visible";
-        //   setTimeout(function(){ document.getElementById("beam").style.visibility = "hidden" }, 1000);
-        //   i=(guesses.length);
-        //   document.getElementById("cow"+[i]).style.visibility = "hidden"
-        //   }
-
-        //   if (((isInArray == true)&&(guesses.includes(String.fromCharCode(tGuess)))===false)
-        //   &&(uniqueLetters.includes(String.fromCharCode(tGuess)))===true){/*selection must be a letter, not already guessed, and in the answer*/
-        //   vanna.push (String.fromCharCode(tGuess)); /*pushes letter from tGuess into "guesses" array*/
-        //   console.log (vanna);
-
-
-        //   if (isInArray == false) {/*ensures only valid guesses are counted*/
-        //     alert ("not valid keystroke");
-
-        // if (guesses.length > 9) { /*Too many guess triggers game's end*/
-        // alert ("You Fail!");
-      
-// function aBar() {
-//   for (var i; i<randomWord.length-1; i++) {document.getElementById('answerBar').innerHTML += "_";}}
-
-// var answerArray = [];
-// for (var i = 0; i < randomWord.length; i++) {
-//   answerArray[i] = "_";
-
-//     const unique = (value, index, self) => {/*creates a list of unique letters in the word called "uniqueLetters" for easy comparison*/
-//       return self.indexOf(value) === index
-//     }
-//     const uniqueLetters = wordSplit.filter(unique)
+//   function answers() {document.getElementById('answerBar').innerHTML +='<img id=space'+[i]+' class="space responsive" src="assets/images/space.png">'
+// }
     
-//     var answerArray = [];
-//       for (var i = 0; i < randomWord.length; i++) {
-//         answerArray[i] = "_";
-//       }
-//     var remainingLetters = randomWord.length;
-
-// /* Capturing guesses. Logs in array "guesses" as keyCodes. */
- 
-//     var letterArray = Array.from(new Array(26), (x,i) => i + 65); /*gets all numbers correlating to letter keycodes and names the variables letterArray */
-
-//     document.onkeyup = letterSelect; /*listens for keystrokes*/
-
-//     var guesses = []; /*empty array filled by letters in randomWord*/
-
-//     var vanna = []; /*empty array filled by guesses that correspond with randomWord*/
-
-
-// /*Captures keycode for key pushed. If valid, adds to variable "guesses". If not, alerts player.*/
-//     document.onkeyup = letterSelect
-// function letterSelect() {
-//     
-//       isInArray = letterArray.includes(tGuess); /*checks that tGuess is a letter (in letterArray)*/
-        
-//         if (((isInArray == true)&&(guesses.includes(String.fromCharCode(tGuess)))===false)
-//               &&(uniqueLetters.includes(String.fromCharCode(tGuess)))===false){/*selection must be a letter, not already guessed, and not in the answer*/
-//         guesses.push (String.fromCharCode(tGuess)); /*pushes letter from tGuess into "guesses" array*/
-         
-//           document.getElementById("beam").style.visibility = "visible";
-//           setTimeout(function(){ document.getElementById("beam").style.visibility = "hidden" }, 1000);
-//           i=(guesses.length);
-//           document.getElementById("cow"+[i]).style.visibility = "hidden"
-//           }
-
-//           if (((isInArray == true)&&(guesses.includes(String.fromCharCode(tGuess)))===false)
-//           &&(uniqueLetters.includes(String.fromCharCode(tGuess)))===true){/*selection must be a letter, not already guessed, and in the answer*/
-//           vanna.push (String.fromCharCode(tGuess)); /*pushes letter from tGuess into "guesses" array*/
-
-//           if (isInArray == false) {/*ensures only valid guesses are counted*/
-//             alert ("not valid keystroke");
-
-//         if (guesses.length > 9) { /*Too many guess triggers game's end*/
-//         alert ("You Fail!");
-
-//       /* Comparing guesses (var guesses) to words (var uniqueLetters)*/
-//       if (wordSplit.includes(String.fromCharCode(tGuess))&&(vanna.includes(String.fromCharCode(tGuess)))===false){
-//         vanna.push(String.fromCharCode(tGuess));h
-//     
-//         if (uniqueLetters.length===vanna.length) {/*comparing lengths of two arrays--no need to check specific characters*/
-//         alert ("You Win!"); 
-//           winCounter++
-
-//         for (var j = 0; j < randomWord.length; j++) {
-//           if (randomWord[j] === guess) {
-//            answerArray[j] = guess;
-//           remainingLetters--;
+  //loading alphabet
+  
+  //  onclick="clicked(this.id)" document.getElementById("'+i+'").src="assets/images/Alphabet/'+alphabet[i]+'b.png">'
+    //'<img class="ibtn" input="" type="image" src="assets/images/Alphabet/'+alphabet[j]+'.png" button id="'+alphabet[i]+'"onclick="document.getElementById("'+i+'").src="assets/images/space.png">'
+  
+  // //loading cows
+  // for (i = 1; i < 10; i++) {document.getElementById('cows').innerHTML += 
+  //   '<img class="cow" id="cow'+i+'" src="assets/images/happy.png" alt="cows">';
+  // }
+  //loading flying saucer and beam (hidden)
